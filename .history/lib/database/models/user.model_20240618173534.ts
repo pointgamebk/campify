123fa95d01888e4901345b533313403a5dc4a811
Brian Hill <gamepointbk@gmail.com>
2024-06-18 17:35:53 -0400

    user model schema

--- .history/lib/database/models/user.model_20240618173534.ts
+++ .history/lib/database/models/user.model_20240618173534.ts
@@ -1,13 +1,18 @@
 import { Document, Schema, model, models } from "mongoose";
 
-export interface IProfile extends Document {}
+export interface IProfile extends Document {
+  profilePhoto: string;
+  profileDescription: string;
+  profileSchool: string;
+  profileContact: string;
+}
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  lastName: { type: String, required: true, default: " " },
   stripeAccountId: { type: String, default: null },
   chargesEnabled: { type: Boolean, default: false },
   photo: { type: String, required: true },
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -12,7 +12,7 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  lastName: { type: String, required: true, default: " " },
   stripeAccountId: { type: String, default: null },
   chargesEnabled: { type: Boolean, default: false },
   photo: { type: String, required: true },
