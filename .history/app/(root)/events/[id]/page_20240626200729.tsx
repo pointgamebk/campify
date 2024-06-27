5eaca0b0871976bf107d528a670a21ce549acc90
Brian Hill <gamepointbk@gmail.com>
2024-06-26 20:15:37 -0400

    debug

--- .history/app/(root)/events/[id]/page_20240626200729.tsx
+++ .history/app/(root)/events/[id]/page_20240626200729.tsx
@@ -1,3 +1,5 @@
+"use server";
+
 import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import {
@@ -17,12 +19,10 @@ const EventDetails = async ({
   searchParams,
 }: SearchParamProps) => {
   try {
-    // const authUser = await currentUser();
-    // const user = await getUserByClerkId(
-    //   authUser?.id || "user_2i4XYyrgQ1Xo9IvjuydCOGCLmZ6"
-    // );
-
-    const user = await getUserByClerkId("user_2i4XYyrgQ1Xo9IvjuydCOGCLmZ6");
+    const authUser = await currentUser();
+    const user = await getUserByClerkId(
+      authUser?.id || "user_2i4XYyrgQ1Xo9IvjuydCOGCLmZ6"
+    );
     const userId = user._id as string;
 
     const event = await getEventById(id);
